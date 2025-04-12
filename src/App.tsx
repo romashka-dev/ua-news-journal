c810a19e407057f1077b2ae767162c9c4182f514
romashka-dev <romikkbox@gmail.com>
2025-04-12 22:59:27 +0300

    [feat] create scss variables + custom hook for dark/light theme on pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import { Suspense } from 'react'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import './styles/index.scss'
 
 import { AboutPageAsync } from './pages/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage.async'
+import { useTheme } from './theme/useTheme'
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Button</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
