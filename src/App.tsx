d44cc1a9e5416a8109b7eb77a0cf8ce01eb9a1f7
romashka-dev <romikkbox@gmail.com>
2025-04-13 10:10:25 +0300

    [feat] create helper classNames + update .gitignore + remove build file

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage.async'
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Button</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
