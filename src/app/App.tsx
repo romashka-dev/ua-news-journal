6a2ad6ba34b95f85aeda7c43e780c6093c67c851
romashka-dev <romikkbox@gmail.com>
2025-04-15 10:07:29 +0300

    [chore] create fsd architecture in project + setup absolut imports in files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { Suspense } from 'react'
 import { Link } from 'react-router-dom'
-import './index.scss'
 import './styles/index.scss'
 
-import { AboutPageAsync } from './pages/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage.async'
-import { useTheme } from './theme/useTheme'
-import { classNames } from './helpers/classNames/classNames'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
+import { useTheme } from 'app/providers/lib/useTheme'
+import { classNames } from 'shared/lib/classNames/classNames'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -19,8 +18,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />}></Route>
-          <Route path="/" element={<MainPageAsync />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/" element={<MainPage />}></Route>
         </Routes>
       </Suspense>
     </div>
