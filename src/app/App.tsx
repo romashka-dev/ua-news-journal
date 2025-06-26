54ebf19fa0d9c894a9f3bd7842e59b675f0ad7f3
romashka-dev <romikkbox@gmail.com>
2025-06-26 14:37:27 +0300

    [feat] create widget Navbar + shared component AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
 import './styles/index.scss'
-import { Link } from 'react-router-dom'
-
-import { useTheme } from 'app/providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AppRouter } from './providers/router'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { AppRouter } from 'app/providers/router'
+import { Navbar } from 'app/widgets/Navbar'
 
 const App = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>Button</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Button</button>
     </div>
   )
 }
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import { Suspense } from 'react'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
